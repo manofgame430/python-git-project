8c6c6ef9f54df988d547ef3acfa6e1b868658b4b
Christopher Labra <clabra27@basecampcodingacademy.org>
2026-08-17 08:49:27 -0500

    Update Python project

--- to-do-list.py
+++ to-do-list.py
@@ -6,14 +6,18 @@ def remove_validation(number:str, list:list):
         else:
             print("Please pick a number on the list")
             number = input("> ")
-    
-        
+
+def finish_task(text):
+    result = "\033[9m" + text + "\033[0m"
+    return result
+
+
 def main():
     while True:
         print("List of things to do:")
         for index, item in enumerate(mylist):
             print(f"{index}. {item}")
-        choice = input("would you like to add an item [1], delete an item [2], or are you done [3]? ")
+        choice = input("would you like to add an item [1], delete an item [2], cross out an item [3], or are you done [4]? ")
         if choice == "1":
             text = input("What would you like to add to the list?\n> ")
             mylist.append(text)
@@ -23,6 +27,10 @@ def main():
             mylist.pop(remove)
             pass
         elif choice == "3":
+            task_done = input("Please insert the number of the task you want to cross out.\n>")
+            task_done = remove_validation(task_done, mylist)
+            mylist[task_done] = finish_task(mylist[task_done])
+        elif choice == "4":
             print("Goodbye")
             break
         else:
